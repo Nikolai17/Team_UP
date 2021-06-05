329f343912581616dad792ee28be6fdca84417ce
Ivan Kopiev <x@mustdev.ru>
2021-06-05 18:11:44 +0300

    Add gradient

--- Team_UP/Team_UP/Competitions/EventCell.swift
+++ Team_UP/Team_UP/Competitions/EventCell.swift
@@ -12,7 +12,7 @@ class EventCell: UITableViewCell {
     let typeLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
-        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.font = UIFont.boldSystemFont(ofSize: 24)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -22,7 +22,7 @@ class EventCell: UITableViewCell {
         label.textColor = .white
         label.numberOfLines = 2
         label.lineBreakMode = .byWordWrapping
-        label.font = UIFont.systemFont(ofSize: 14)
+        label.font = UIFont.systemFont(ofSize: 15)
         label.translatesAutoresizingMaskIntoConstraints = false
 
         return label
@@ -36,13 +36,24 @@ class EventCell: UITableViewCell {
         iv.translatesAutoresizingMaskIntoConstraints = false
         return iv
     }()
+    let gradientImageView: UIImageView = {
+        let iv = UIImageView(image: UIImage(named: "gradient"))
+        iv.clipsToBounds = true
+        iv.contentMode = .scaleAspectFill
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
     
     let viewConteiner: UIView = {
         let view = UIView()
         view.backgroundColor = .white
         view.layer.cornerRadius = 10
         view.translatesAutoresizingMaskIntoConstraints = false
-        
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
+        view.layer.shadowRadius = 3.0
+        view.layer.shadowOpacity = 0.2
+        view.layer.masksToBounds = false
        return view
     }()
     
@@ -72,31 +83,21 @@ class EventCell: UITableViewCell {
         contentView.backgroundColor = .clear
         backgroundColor = .clear
         addSubview(eventImageView)
-        eventImageView.addSubview(typeLabel)
-        eventImageView.addSubview(adressLabel)
-        eventImageView.addSubview(viewConteiner)
+        eventImageView.addSubview(gradientImageView)
+        gradientImageView.addSubview(typeLabel)
+        gradientImageView.addSubview(adressLabel)
+        gradientImageView.addSubview(viewConteiner)
         viewConteiner.addSubview(dateLabel)
-        let gradient: CAGradientLayer = {
-            let layer = CAGradientLayer()
-            layer.colors = [
-              UIColor(red: 0.035, green: 0.035, blue: 0.188, alpha: 0).cgColor,
-              UIColor(red: 0.035, green: 0.035, blue: 0.188, alpha: 1).cgColor
-            ]
-
-            layer.locations = [0.39, 1]
-            layer.startPoint = CGPoint(x: 0.25, y: 0.5)
-            layer.endPoint = CGPoint(x: 0.75, y: 0.5)
-            layer.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: 0, b: 1, c: -1, d: 0, tx: 1, ty: 0))
-            layer.bounds = eventImageView.bounds.insetBy(dx: -0.5*eventImageView.bounds.size.width, dy: -0.5*eventImageView.bounds.size.height)
-            layer.position = eventImageView.center
-            return layer
-        }()
-        eventImageView.layer.addSublayer(gradient)
         NSLayoutConstraint.activate([
             eventImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
             eventImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             eventImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
             eventImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            gradientImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            gradientImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            gradientImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            gradientImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
             // box
             viewConteiner.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             viewConteiner.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor),
@@ -108,7 +109,7 @@ class EventCell: UITableViewCell {
             dateLabel.topAnchor.constraint(equalTo: viewConteiner.topAnchor, constant: 4),
             dateLabel.bottomAnchor.constraint(equalTo: viewConteiner.bottomAnchor, constant: -4),
             // Type
-            typeLabel.heightAnchor.constraint(equalToConstant: 30),
+            typeLabel.heightAnchor.constraint(equalToConstant: 25),
             typeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             typeLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             typeLabel.bottomAnchor.constraint(equalTo: adressLabel.topAnchor, constant: -10),
@@ -116,8 +117,6 @@ class EventCell: UITableViewCell {
             adressLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             adressLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             adressLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30),
-            adressLabel.heightAnchor.constraint(equalToConstant: 30)
-
         ])
     }
     
